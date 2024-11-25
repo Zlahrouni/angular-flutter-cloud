71dc4d65a0f4151a059ea654763bbd8b0392194f
Zlahrouni <ziad.lahrouni@gmail.com>
2024-11-25 10:58:00 +0100

    init front

--- front-angular/src/app/app.module.ts
+++ front-angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavarComponent } from './components/navar/navar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
