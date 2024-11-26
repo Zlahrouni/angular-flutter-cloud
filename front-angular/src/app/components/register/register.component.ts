479d22a712e07d4496e8dc0865df3b385d7a0dc4
mohamedkerraz <mohamedreda.kerraz@gmail.com>
2024-11-26 09:44:50 +0100

    add double mdp

--- front-angular/src/app/components/register/register.component.ts
+++ front-angular/src/app/components/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'tm-register',
@@ -15,11 +15,30 @@ export class RegisterComponent {
     private router: Router,
     private fb: FormBuilder
   ) {
-    this.registerForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
-    });
+      this.registerForm = this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]]
+    }, { validator: this.matchPasswords });
   }
+
+  get email() { return this.registerForm.get('email'); }
+  get password() { return this.registerForm.get('password'); }
+  get confirmPassword() { return this.registerForm.get('confirmPassword'); }
+
+  matchPasswords(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+
+    if (password && confirmPassword && password.value !== confirmPassword.value) {
+      // Définit l'erreur sur le champ confirmPassword
+      confirmPassword.setErrors({ passwordMismatch: true });
+      return { passwordMismatch: true };
+    }
+
+    return null;
+  }
+
 
   async onSubmit() {
     if (this.registerForm.valid) {
